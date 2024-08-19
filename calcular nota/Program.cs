85a56d51d6c730ddb58ac0a11a80538faf5907c1
ChristopherMaldonado24 <saidreyes711@gmail.com>
2024-08-19 13:18:51 -0600

    Actualizacion
    Agregado de lineas

--- calcular nota/Program.cs
+++ calcular nota/Program.cs
@@ -2,44 +2,52 @@
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("bienvenidos al progarama calculador de notas");
+        Console.WriteLine(" bienvenidos al progarama calculador de notas ");
 
-        Console.WriteLine("ingrese la nota del primer parcial");
+        Console.WriteLine(" Ingrese su nombre ");
+        string Nombre = Console.ReadLine();
+
+        Console.WriteLine(" Ingrese el nombre de la materia ");
+        string Asignatura = Console.ReadLine();
+
+        Console.WriteLine(" ingrese la nota del primer parcial ");
         int N1 = Convert.ToInt32(Console.ReadLine());
 
-        Console.WriteLine("ingrese la nota del segundo parcial");
+        Console.WriteLine(" ingrese la nota del segundo parcial ");
         int N2 = Convert.ToInt32(Console.ReadLine());
 
-        Console.WriteLine("ingrese la nota del tercer parcial");
-        int N3 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine(" ingrese la nota del tercer parcial ");
+        int N3 = Convert.ToInt32(Console.ReadLine()); 
 
-        Console.WriteLine("ingrese la nota del cuarto parcial");
+        Console.WriteLine(" ingrese la nota del cuarto parcial ");
         int N4 = Convert.ToInt32(Console.ReadLine());
 
-        double Total = N1 + N2 + N3 + N4;
+        double Total = (N1 + N2 + N3 + N4)/4;
 
-        double Final = Total / 4;
-
-        Console.WriteLine(" su promerdio es de: " + Final);
+        Console.WriteLine(" su promerdio es de: " + Total);
         Console.WriteLine(" Observación ");
-        if (Final < 70)
+        if (Total < 70)
         {
             Console.WriteLine(" Usted esta Reprobado ");
+            Console.ReadKey();
         }
 
-        if (Final > 70 & Final < 81 )
+        if (Total > 70 & Total < 81 )
         {
             Console.WriteLine(" Bueno ");
+            Console.ReadKey();
         }
 
-        if (Final > 81 & Final < 91 )
+        if (Total > 81 & Total < 91 )
         {
             Console.WriteLine(" Muy Bueno ");
+            Console.ReadKey();
         }
         
-        if (Final > 91)
+        if (Total > 91)
         {
             Console.WriteLine(" Excelente ");
+            Console.ReadKey();
         }
     }
 }
